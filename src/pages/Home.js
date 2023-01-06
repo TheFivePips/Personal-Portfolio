66da81f50a19b07610582a95bb6894dcf046e040
Sam Gardner <samgardner20@gmail.com>
2023-01-06 12:57:03 -0500

    added in the very basics of a about/home page with an image and styled components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,43 @@
 import React from "react";
+// import styled from "styled-components";
 import Nav from "../comonents/Nav";
+import Coffee from "../assets/coffee.jpg"
+import styled from "styled-components";
 
 const Home = () => {
+
+  const Section = styled.section`
+    width: 850px;
+    height 700px;
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+    // border: 1px solid black;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: center;
+    
+  `;
+  const PPicture = styled.img`
+    height: 100%;
+    width: 100%;
+    border-radius: 1rem;
+    
+
+  `
+  const Para = styled.p`
+    line-height: 2.5rem;
+    text-align: center;
+  `;
+    
+
   return (
     <div>
       <Nav />
-      <p>Home</p>
+      <Section>
+        <PPicture src={Coffee} alt="picutre of me" />
+        <Para>Hello, there! My name is Sam <br/>I am a Full-Stack Web Developer</Para>
+      </Section>
     </div>
   );
 };
